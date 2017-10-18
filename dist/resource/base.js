a508c1fd7b6c1646cee8943ee12413e23a10e512
William Pelrine <rudy@ionic.io>
2017-10-18 10:32:30 -0500

    Better, ingredients, better BaseResource!

--- dist/resource/base.js
+++ dist/resource/base.js
@@ -3,9 +3,49 @@ export class BaseResource {
         this.endpoint = endpoint;
         this.api = api;
     }
-    del(pk) { return this.api.del(this.endpoint + pk); }
-    get(pk) { return this.api.get(this.endpoint + pk); }
-    list() { return this.api.get(this.endpoint); }
-    patch(pk, body) { return this.api.patch(this.endpoint + pk, body); }
-    post(body) { return this.api.post(this.endpoint, body); }
+    del(pk) {
+        return new Promise((resolve, reject) => {
+            this.api.del(this.endpoint + pk).then((res) => {
+                resolve(res.data);
+            }, (err) => {
+                reject(err.error || { "error": "Unknown" });
+            });
+        });
+    }
+    get(pk) {
+        return new Promise((resolve, reject) => {
+            this.api.get(this.endpoint + pk).then((res) => {
+                resolve(res.data);
+            }, (err) => {
+                reject(err.error || { "error": "Unknown" });
+            });
+        });
+    }
+    list() {
+        return new Promise((resolve, reject) => {
+            this.api.get(this.endpoint).then((res) => {
+                resolve(res.data);
+            }, (err) => {
+                reject(err.error || { "error": "Unknown" });
+            });
+        });
+    }
+    patch(pk, body) {
+        return new Promise((resolve, reject) => {
+            this.api.patch(this.endpoint + pk, body).then((res) => {
+                resolve(res.data);
+            }, (err) => {
+                reject(err.error || { "error": "Unknown" });
+            });
+        });
+    }
+    post(body) {
+        return new Promise((resolve, reject) => {
+            this.api.post(this.endpoint, body).then((res) => {
+                resolve(res.data);
+            }, (err) => {
+                reject(err.error || { "error": "Unknown" });
+            });
+        });
+    }
 }
--- src/resource/base.ts
+++ src/resource/base.ts
@@ -1,16 +1,56 @@
-import { Api } from '../util/api'
+import { Api, ApiResponse } from '../util/api'
 
 export class BaseResource {
 
     constructor(public endpoint: string, public api: Api) {}
 
-    del(pk: string) { return this.api.del(this.endpoint + pk); }
+    del(pk: string) {
+      return new Promise((resolve, reject) => {
+        this.api.del(this.endpoint + pk).then((res: ApiResponse) => {
+          resolve(res.data);
+        }, (err: ApiResponse) => {
+          reject(err.error || {"error": "Unknown"});
+        });
+      });
+    }
 
-    get(pk: string) { return this.api.get(this.endpoint + pk); }
+    get(pk: string) { 
+      return new Promise((resolve, reject) => {
+        this.api.get(this.endpoint + pk).then((res: ApiResponse) => {
+          resolve(res.data);
+        }, (err: ApiResponse) => {
+          reject(err.error || {"error": "Unknown"});
+        });
+      });
+    }
 
-    list() { return this.api.get(this.endpoint); }
+    list() { 
+      return new Promise((resolve, reject) => {
+        this.api.get(this.endpoint).then((res: ApiResponse) => {
+          resolve(res.data);
+        }, (err: ApiResponse) => {
+          reject(err.error || {"error": "Unknown"});
+        });
+      });
+     }
 
-    patch(pk: string, body: any) {return this.api.patch(this.endpoint + pk, body); }
+    patch(pk: string, body: any) {
+      return new Promise((resolve, reject) => {
+        this.api.patch(this.endpoint + pk, body).then((res: ApiResponse) => {
+          resolve(res.data);
+        }, (err: ApiResponse) => {
+          reject(err.error || {"error": "Unknown"});
+        });
+      });
+    }
 
-    post(body: any) { return this.api.post(this.endpoint, body); }
+    post(body: any) { 
+      return new Promise((resolve, reject) => {
+        this.api.post(this.endpoint, body).then((res: ApiResponse) => {
+          resolve(res.data);
+        }, (err: ApiResponse) => {
+          reject(err.error || {"error": "Unknown"});
+        });
+      });
+    }
 }
