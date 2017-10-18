b923e635277c68cb22261170b57593d35fb4de7e
Lars Mikkelsen <lars@ionic.io>
2017-10-18 12:35:51 -0500

    Add Apps, Org, OrgInvite types

--- src/resource/apps.ts
+++ src/resource/apps.ts
@@ -9,8 +9,63 @@ export interface DashMetadata {
   data: any;
 }
 
-export class AppsResource extends BaseResource {
-  constructor(api: Api) { super('/apps', api); }
+export interface OrgInvite {
+}
+
+export interface Org {
+  id: string;
+  name: string;
+  slug: string;
+  seats: number;
+  avatar: string;
+  description: string;
+  email: string;
+  created: string;
+  plan: string;
+  stripe_customer: boolean;
+  invites: Array<OrgInvite>;
+}
+
+export interface App {
+  id: string;
+  name: string;
+  slug: string;
+  owner: number;
+  org_id: string;
+  created: string;
+  updated: string;
+  plan: string;
+  icon: string;
+  org: Org;
+  repo_url: string;
+}
+
+export class AppsResource {
+  private _base: BaseResource
+
+  constructor(api: Api) {
+    this._base = new BaseResource('/apps/', api);
+  }
+
+  del(pk: string, params?: any): Promise<App> {
+    return this._base.del(pk, params);
+  }
+
+  get(pk: string): Promise<App> {
+    return this._base.get(pk);
+  }
+
+  list(params?: any): Promise<Array<App>> {
+    return this._base.list(params);
+  }
+
+  patch(pk: string, body: App): Promise<App> {
+    return this._base.patch(pk, body);
+  }
+
+  post(body: App): Promise<App> {
+    return this._base.post(body);
+  }
 
   getDashMeta(pk: string): Promise<any> {
     return this.get(pk + '/dash-metadata');
@@ -21,12 +76,12 @@ export class AppsResource extends BaseResource {
   }
 
   setDashMeta(pk: string, meta: DashMetadata): Promise<any> {
-    return this.patch(pk + '/dash-metadata', meta);
+    return this._base.patch(pk + '/dash-metadata', meta);
   }
 
   transfer(pk: string, to: AppTransfer): Promise<any> {
     return new Promise((resolve, reject) => {
-      this.api.post(this.endpoint + '/' + pk, to).then((res: ApiResponse) => {
+      this._base.api.post(this._base.endpoint + '/' + pk, to).then((res: ApiResponse) => {
         resolve(res.data);
       }, (err: ApiResponse) => {
         reject(err.error || {"error": "Unknown"});
