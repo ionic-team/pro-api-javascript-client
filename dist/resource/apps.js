2f67c3e15435c8edaf3a4dbcf49092f91ad6c4b2
William Pelrine <rudy@ionic.io>
2017-10-18 13:32:17 -0500

    Types for resources in dist

--- dist/resource/apps.js
+++ dist/resource/apps.js
@@ -1,6 +1,23 @@
 import { BaseResource } from './base';
-export class AppsResource extends BaseResource {
-    constructor(api) { super('/apps', api); }
+export class AppsResource {
+    constructor(api) {
+        this._base = new BaseResource('/apps/', api);
+    }
+    del(pk, params) {
+        return this._base.del(pk, params);
+    }
+    get(pk) {
+        return this._base.get(pk);
+    }
+    list(params) {
+        return this._base.list(params);
+    }
+    patch(pk, body) {
+        return this._base.patch(pk, body);
+    }
+    post(body) {
+        return this._base.post(body);
+    }
     getDashMeta(pk) {
         return this.get(pk + '/dash-metadata');
     }
@@ -8,11 +25,11 @@ export class AppsResource extends BaseResource {
         return this.get('/slug/' + slug);
     }
     setDashMeta(pk, meta) {
-        return this.patch(pk + '/dash-metadata', meta);
+        return this._base.patch(pk + '/dash-metadata', meta);
     }
     transfer(pk, to) {
         return new Promise((resolve, reject) => {
-            this.api.post(this.endpoint + '/' + pk, to).then((res) => {
+            this._base.api.post(this._base.endpoint + '/' + pk, to).then((res) => {
                 resolve(res.data);
             }, (err) => {
                 reject(err.error || { "error": "Unknown" });
--- dist/resource/snapshot.d.ts
+++ dist/resource/snapshot.d.ts
@@ -4,13 +4,38 @@ export interface SnapshotListArgs {
     users?: Array<number>;
     ref?: string;
 }
+export interface User {
+}
+export interface SnapshotIOSVersion {
+    min: string;
+    max: string;
+    eq: string;
+}
+export interface SnapshotAndroidVersion {
+    min: string;
+    max: string;
+    eq: string;
+}
+export interface Snapshot {
+    id: string;
+    note: string;
+    sha: string;
+    ref: string;
+    ref_type: string;
+    state: string;
+    user: User;
+    ios_version: SnapshotIOSVersion;
+    android_version: SnapshotAndroidVersion;
+    created: string;
+    short_sha: string;
+}
 export declare class SnapshotsResource {
     private _base;
     constructor(api: Api);
-    del(appId: string, pk: string): Promise<any>;
-    get(appId: string, pk: string): Promise<any>;
+    del(appId: string, pk: string): Promise<Snapshot>;
+    get(appId: string, pk: string): Promise<Snapshot>;
     getUrl(appId: string, pk: string): Promise<any>;
-    list(appId: string, params?: SnapshotListArgs): Promise<any>;
-    patch(appId: string, pk: string, body: any): Promise<any>;
-    post(appId: string, body: any): Promise<any>;
+    list(appId: string, params?: SnapshotListArgs): Promise<Array<Snapshot>>;
+    patch(appId: string, pk: string, body: Snapshot): Promise<Snapshot>;
+    post(appId: string, body: Snapshot): Promise<Snapshot>;
 }
