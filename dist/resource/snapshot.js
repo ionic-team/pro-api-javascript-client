5871460f739a9d8b31405352662a393dd0a39dce
William Pelrine <rudy@ionic.io>
2017-10-18 11:42:20 -0500

    Refactor snapshot endpoints

--- dist/resource/snapshot.js
+++ dist/resource/snapshot.js
@@ -4,34 +4,23 @@ export class SnapshotsResource {
         this._base = new BaseResource('/apps/', api);
     }
     del(appId, pk, params) {
-        return this._base.del(appId + '/snapshots/' + pk, params);
+        this._base.endpoint = '/apps/' + appId + '/snapshots';
+        return this._base.del(pk, params);
     }
     get(appId, pk, params) {
-        return this._base.get(appId + '/snapshots/' + pk, params);
+        this._base.endpoint = '/apps/' + appId + '/snapshots';
+        return this._base.get(pk, params);
     }
     list(appId, params) {
-        let queryString = appId + '/snapshots';
-        if (params) {
-            queryString += (queryString.indexOf('?') === -1 ? '?' : '&') + Object.keys(params).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])).join('&');
-        }
-        return new Promise((resolve, reject) => {
-            this._base.api.get(this._base.endpoint + queryString).then((res) => {
-                resolve(res.data);
-            }, (err) => {
-                reject(err.error || { "error": "Unknown" });
-            });
-        });
+        this._base.endpoint = '/apps/' + appId + '/snapshots';
+        return this._base.list(params);
     }
     patch(appId, pk, body) {
-        return this._base.patch(appId + '/snapshots/' + pk, body);
+        this._base.endpoint = '/apps/' + appId + '/snapshots';
+        return this._base.patch(pk, body);
     }
     post(appId, body) {
-        return new Promise((resolve, reject) => {
-            this._base.api.post(this._base.endpoint + appId + '/snapshots', body).then((res) => {
-                resolve(res.data);
-            }, (err) => {
-                reject(err.error || { "error": "Unknown" });
-            });
-        });
+        this._base.endpoint = '/apps/' + appId + '/snapshots';
+        return this._base.post(body);
     }
 }
--- src/resource/snapshot.ts
+++ src/resource/snapshot.ts
@@ -1,4 +1,4 @@
-import { Api, ApiResponse } from '../util/api';
+import { Api } from '../util/api';
 import { BaseResource } from './base';
 
 export interface SnapshotListArgs {
@@ -15,38 +15,27 @@ export class SnapshotsResource {
   }
 
   del(appId: string, pk: string, params?: any): Promise<any> {
-    return this._base.del(appId + '/snapshots/' + pk, params);
+    this._base.endpoint = '/apps/' + appId + '/snapshots';
+    return this._base.del(pk, params);
   }
 
   get(appId: string, pk: string, params?: any): Promise<any> {
-    return this._base.get(appId + '/snapshots/' + pk, params);
+    this._base.endpoint = '/apps/' + appId + '/snapshots';
+    return this._base.get(pk, params);
   }
 
   list(appId: string, params?: any): Promise<any> {
-    let queryString = appId + '/snapshots';
-    if (params) {
-      queryString += (queryString.indexOf('?') === -1 ? '?' : '&') + Object.keys(params).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])).join('&');
-    }
-    return new Promise((resolve, reject) => {
-      this._base.api.get(this._base.endpoint + queryString).then((res: ApiResponse) => {
-        resolve(res.data);
-      }, (err: ApiResponse) => {
-        reject(err.error || {"error": "Unknown"});
-      });
-    });
+    this._base.endpoint = '/apps/' + appId + '/snapshots';
+    return this._base.list(params);
   }
 
   patch(appId: string, pk: string, body: any): Promise<any> {
-    return this._base.patch(appId + '/snapshots/' + pk, body);
+    this._base.endpoint = '/apps/' + appId + '/snapshots';
+    return this._base.patch(pk, body);
   }
 
   post(appId: string, body: any): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this._base.api.post(this._base.endpoint + appId + '/snapshots', body).then((res: ApiResponse) => {
-        resolve(res.data);
-      }, (err: ApiResponse) => {
-        reject(err.error || {"error": "Unknown"});
-      });
-    });
+    this._base.endpoint = '/apps/' + appId + '/snapshots';
+    return this._base.post(body);
   }
 }
